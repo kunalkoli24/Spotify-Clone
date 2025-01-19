9179daf59e9dfe7376e5111e0d03942bf551cb6c
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-01-19 13:39:44 +0530

    navbar

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styled from "styled-components";
 import Sidebar from './Sidebar';
+import { useStateprovider } from "../utils/stateprovider";
+
 import Body from './Body';
 import NavBar from './NavBar';
 import Footer from './Footer';
+import axios from 'axios';
+import { reducerCases } from '../utils/Constants';
 export default function Spotify() {
+    const [{ token }, dispatch] = useStateprovider();
+
+    useEffect(() =>{
+        const getUserInfo = async ()=>{
+            const {data} = await axios.get("https://api.spotify.com/v1/me", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                  },
+            });
+            const userInfo = {
+                userId: data.id,
+                userName: data.display_name,
+            };
+            console.log(userInfo);
+            dispatch({type:reducerCases.SET_USER,userInfo});
+        };
+        getUserInfo();
+    }, [dispatch,token]);
+
   return <Container>
     <div className="spotifybody">
         <Sidebar />
